e6f12300af662964c6029f7517d64d8ce28d366e
taha <nanecitaha@gmail.com>
2020-05-08 01:23:15 -0500

    first commit

---
+++ src/test/java/tahatest/AppiumTest.java
@@ -0,0 +1,15 @@
+package tahatest;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+    @Test
+    public void NativeAppAndroid(){
+        System.out.println("NativeAppAndroid");
+    }
+    @Test
+    public void IOSApps(){
+        System.out.println("IOSApps");
+    }
+}
+
---
+++ src/test/java/tahatest/RESTApiTest.java
@@ -0,0 +1,18 @@
+package tahatest;
+
+
+import org.testng.annotations.Test;
+
+;
+
+
+public class RESTApiTest {
+    @Test
+    public void postJira(){
+        System.out.println("postJira");
+    }
+    @Test
+    public void deleteTwitter(){
+        System.out.println("deleteTwitter");
+    }
+}
